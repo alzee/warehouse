2d2acdd5c65919839286a30540c696952ab0c84e
Al Zee <z@alz.ee>
2021-02-26 02:16:42 +0800

    entity zone

---
+++ src/Entity/Zone.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ZoneRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ZoneRepository::class)
+ */
+class Zone
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=3)
+     */
+    private $zone;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $warehouse;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getZone(): ?string
+    {
+        return $this->zone;
+    }
+
+    public function setZone(string $zone): self
+    {
+        $this->zone = $zone;
+
+        return $this;
+    }
+
+    public function getWarehouse(): ?int
+    {
+        return $this->warehouse;
+    }
+
+    public function setWarehouse(int $warehouse): self
+    {
+        $this->warehouse = $warehouse;
+
+        return $this;
+    }
+}
