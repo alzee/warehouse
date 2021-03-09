77fb1506d04c3c893fd72e697e3dd3106f4a5cc7
Al Zee <z@alz.ee>
2021-03-10 01:49:19 +0800

    main menus

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -32,24 +32,32 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('首页', 'fa fa-home');
+        yield MenuItem::linktoDashboard('数据统计', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
         
-        yield MenuItem::section('Section A');
-        yield MenuItem::linktoCrud('类别', 'fa fa-home', Category::class);
-        yield MenuItem::linkToCrud('Add Category', 'fa fa-tags', Category::class)->setAction('new');
-        yield MenuItem::linkToCrud('Show Main Category', 'fa fa-tags', Category::class)->setAction('detail');
-        yield MenuItem::linkToCrud('Show Main Category', 'fa fa-tags', Category::class)->setAction('detail')->setEntityId(1);
-
-        yield MenuItem::section('Section B');
-        yield MenuItem::linktoCrud('类别', 'fa fa-home', Category::class);
-
-        yield MenuItem::subMenu('Blog', 'fa fa-article')->setSubItems([
-            MenuItem::linkToCrud('Categories', 'fa fa-tags', Category::class),
-            MenuItem::linkToCrud('Categories', 'fa fa-tags', Category::class),
+        yield MenuItem::subMenu('仓库管理 ', 'fa fa-tags')->setSubItems([
+            MenuItem::linkToCrud('1号仓库', '', Category::class),
+            MenuItem::linkToCrud('2号仓库', '', Category::class),
         ]);
 
-        yield MenuItem::section('Section Z');
+        yield MenuItem::subMenu('物品管理 ', 'fa fa-tags')->setSubItems([
+            MenuItem::linkToCrud('物品分类', '', Category::class),
+            MenuItem::linkToCrud('物品列表', '', Category::class),
+        ]);
+
+        yield MenuItem::subMenu('物品领用', 'fa fa-tags')->setSubItems([
+            MenuItem::linkToCrud('新建出库单', '', Category::class),
+            MenuItem::linkToCrud('物品使用中', '', Category::class),
+            MenuItem::linkToCrud('物品归还', '', Category::class),
+        ]);
+
+        yield MenuItem::subMenu('仓库日志', 'fa fa-tags')->setSubItems([
+            MenuItem::linkToCrud('入库', '', Category::class),
+            MenuItem::linkToCrud('领用', '', Category::class),
+            MenuItem::linkToCrud('归还', '', Category::class),
+            MenuItem::linkToCrud('损耗', '', Category::class),
+        ]);
+
         //yield MenuItem::linkToLogout('Logout', 'fa fa-exit');
     }
 
