91c68cfe342c321eec70482876349174bda0af5e
Al Zee <z@alz.ee>
2021-03-10 03:39:24 +0800

    category contorller

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -15,23 +15,7 @@ class CategoryCrudController extends AbstractCrudController
 
     public function configureCrud(Crud $crud): Crud
     {
-        return $crud
-            // the visible title at the top of the page and the content of the <title> element
-            // it can include these placeholders: %entity_id%, %entity_label_singular%, %entity_label_plural%
-            ->setPageTitle('index', '%entity_label_plural% listing')
-
-        // you can pass a PHP closure as the value of the title
-            ->setPageTitle('new', fn () => new \DateTime('now') > new \DateTime('today 13:00') ? 'New dinner' : 'New lunch')
-
-        // in DETAIL and EDIT pages, the closure receives the current entity
-        // as the first argument
-            ->setPageTitle('detail', fn (Product $product) => (string) $product)
-            ->setPageTitle('edit', fn (Category $category) => sprintf('Editing <b>%s</b>', $category->getName()))
-
-        // the help message displayed to end users (it can contain HTML tags)
-            ->setHelp('edit', '...')
-            ->setSearchFields(null)
-        ;
+        return $crud;
     }
 
     /*
