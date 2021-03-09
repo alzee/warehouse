c7e169d9bb74d8ef832f89cfcc77bb01eac1ee8f
Al Zee <z@alz.ee>
2021-03-10 07:47:41 +0800

    fix migration

--- migrations/Version20210309231742.php
+++ migrations/Version20210309231742.php
@@ -20,6 +20,7 @@ final class Version20210309231742 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE back ADD take_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE back ADD CONSTRAINT FK_6DCEC1373DF9AAF6 FOREIGN KEY (take_id) REFERENCES take (id)');
         $this->addSql('CREATE INDEX IDX_6DCEC1373DF9AAF6 ON back (take_id)');
     }
