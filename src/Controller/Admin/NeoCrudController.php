899c5a2c87901d33103d4c5e4369bff81423bce3
Al Zee <z@alz.ee>
2021-03-10 06:11:03 +0800

    configure field for neo crud

--- src/Controller/Admin/NeoCrudController.php
+++ src/Controller/Admin/NeoCrudController.php
@@ -11,6 +11,9 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
 class NeoCrudController extends AbstractCrudController
 {
@@ -22,10 +25,19 @@ class NeoCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IntegerField::new('quantity'),
-            AssociationField::new('category'),
+            IntegerField::new('id')->onlyOnIndex(),
             AssociationField::new('item'),
+            AssociationField::new('category'),
             AssociationField::new('zone'),
+            IntegerField::new('quantity'),
+            DateTimeField::new('date')->onlyOnIndex(),
         ];
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->disable(Action::EDIT)
+        ;
     }
 }
