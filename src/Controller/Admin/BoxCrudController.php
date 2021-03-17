51b20a512fd5be6d7a918ae05f9dfaf8ed88a07e
Al Zee <z@alz.ee>
2021-03-17 22:29:32 +0800

    {}号箱子

--- src/Controller/Admin/BoxCrudController.php
+++ src/Controller/Admin/BoxCrudController.php
@@ -6,6 +6,9 @@ use App\Entity\Box;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
 class BoxCrudController extends AbstractCrudController
 {
@@ -20,5 +23,21 @@ class BoxCrudController extends AbstractCrudController
             IdField::new('id')->onlyOnIndex(),
             TextField::new('category'),
         ];
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            //->add(Crud::PAGE_EDIT, Action::SAVE_AND_ADD_ANOTHER)
+        ;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            //->setPageTitle('index', '%entity_label_plural% list');
+            ->setPageTitle('detail', fn (Box $box) => (string) $box . '号箱子');
+            ;
     }
 }
