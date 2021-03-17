5534cbc6f23bf6c99956f890af8e91fd4c43d8d0
Al Zee <z@alz.ee>
2021-03-17 23:59:58 +0800

    redirect to a filtered page is a good idea

--- src/Controller/Admin/BoxCrudController.php
+++ src/Controller/Admin/BoxCrudController.php
@@ -9,6 +9,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 
 class BoxCrudController extends AbstractCrudController
 {
@@ -25,10 +26,19 @@ class BoxCrudController extends AbstractCrudController
         ];
     }
 
+    public function boxEntries(AdminContext $context)
+    {
+        $id = $context->getEntity()->getInstance()->getId();
+        return $this->redirect('http://warehouse/admin?referrer=%2Fadmin%3FcrudAction%3Dindex%26crudControllerFqcn%3DApp%255CController%255CAdmin%255CEntryCrudController%26menuIndex%3D1%26signature%3DaiE8iHC8HTQiY4X8doxA0ZDCcg_f17Rpqm8K9m2DlX0%26submenuIndex%3D3&crudAction=index&crudControllerFqcn=App%5CController%5CAdmin%5CEntryCrudController&menuIndex=1&signature=aiE8iHC8HTQiY4X8doxA0ZDCcg_f17Rpqm8K9m2DlX0&submenuIndex=3&filters%5Bbox%5D%5Bcomparison%5D=%3D&filters%5Bbox%5D%5Bvalue%5D=' . $id);
+    }
+
     public function configureActions(Actions $actions): Actions
     {
+        $boxEntries = Action::new('boxEntries', 'Invoice')->linkToCrudAction('boxEntries');
+
         return $actions
             ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->add(Crud::PAGE_INDEX, $boxEntries);
             //->add(Crud::PAGE_EDIT, Action::SAVE_AND_ADD_ANOTHER)
         ;
     }
