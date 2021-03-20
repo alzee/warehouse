8880e58f5e59b64b3b4edb8ac0be2f1202920d9d
Al Zee <z@alz.ee>
2021-03-20 11:43:25 +0800

    date default now() in sql to aovid doctrine migration automatically remove default current_timestamp

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -1,16 +1,10 @@
 use rfidsystem;
 
-drop trigger sync_record;
+drop trigger if exists sync_record;
 
 delimiter $$
 create trigger sync_record after insert on jilu for each row
 begin
     update warehouse.box set status = 1 - status where id = (select number from bangding where address = new.address);
-    insert into warehouse.log (box, direction) values ((select number from bangding where address = new.address), (select status from warehouse.box where id = (select number from bangding where address = new.address)));
+    insert into warehouse.log (date, box, direction) values (now(), (select number from bangding where address = new.address), (select status from warehouse.box where id = (select number from bangding where address = new.address)));
 end $$
-
--- delimiter $$
--- create trigger sync_asso after insert on bangding for each row
--- begin
---     insert into warehouse.rfid (rfid, box) values (new.address, new.number);
--- end $$
